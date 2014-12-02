005c7e2ad6e83450b0f74eb30a232737d44c9a83
orizilber <olizilber97@gmail.com>
2014-12-02 12:32:07 +0200

    hi

---
+++ recursivethings.cs
@@ -0,0 +1,36 @@
+class recurs
+{
+    static int Main(string[] args)
+    {
+
+    }
+
+    static bool isNumbersEven(int n)
+    {
+        if(n.Length <= 0)
+            return true;
+
+
+
+    }
+
+    static int[] SOPEx(int[] arr)
+    {
+        int[] arr2 = new int[];
+        int j = 0;
+
+        for(i = 0 , i <= arr.Length , i++)
+        {
+            if(arr[i]%2 == 0 || arr[i]%3 == 0)
+                arr2[j] = arr[i];
+                j++;
+        }
+        return arr2;
+    }
+
+    static sumOfPrimes(int[] arr, int i)
+    {
+        arr2 = SOPEx(arr);
+
+    }
+}
