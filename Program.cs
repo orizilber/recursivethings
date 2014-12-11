3123dc18cc5e66e32ab37c788ff121be7d3d4d38
orizilber <olizilber97@gmail.com>
2014-12-11 18:04:06 +0200

    hi

--- Program.cs
+++ Program.cs
@@ -15,6 +15,10 @@ namespace Recursion_examples
             Console.WriteLine(IsIncreasingOrder(arr));
             Console.WriteLine(LowercaseLetters("ABRAKADABRA"));
             Console.WriteLine(ReversedWord("ABRAKADABRA"));
+
+            //int[] arr = new int[] {1, 4, -2, 7, 99, 0};
+            //Console.WriteLine(Max(arr, arr.Length-1));
+            Console.WriteLine(Factorial(10));
         }
 
         static bool IsAllSameParity(int n)
@@ -39,7 +43,7 @@ namespace Recursion_examples
             }
         }
 
-        static int SumOfFirst(int[] arr , int i) 
+        static int SumOfFirst(int[] arr , int i)
         {
             if (i == 0)
                 return arr[0];
@@ -47,13 +51,13 @@ namespace Recursion_examples
             return SumOfFirst(arr, i-1) + arr[i];
         }
 
-        static bool IsIncreasingOrder(int[] arr) 
+        static bool IsIncreasingOrder(int[] arr)
         {
             int i = arr.Length;
             return IsIncreasingOrderInner(arr, i-1);
         }
 
-        //static bool IsIncreasingOrderInner(int[] arr, int i) 
+        //static bool IsIncreasingOrderInner(int[] arr, int i)
         //{
         //    if(i == 0)
         //    {
@@ -79,7 +83,7 @@ namespace Recursion_examples
             return LowercaseLettersInner(word, word.Length - 1);
         }
 
-        static int LowercaseLettersInner(string word, int i) 
+        static int LowercaseLettersInner(string word, int i)
         {
             if (i == 0)
             {
@@ -117,23 +121,99 @@ namespace Recursion_examples
             //}
         }
 
-        static string ReversedWord(string word) 
+        static string ReversedWord(string word)
         {
             //return ReversedWordInner(word, word.Length - 1);
 
             if (word.Length == 1)
                 return word;
 
-            return word[word.Length - 1] + ReversedWord(word.Remove(word.Length - 1)); 
+            return word[word.Length - 1] + ReversedWord(word.Remove(word.Length - 1));
         }
 
-        static string ReversedWordInner(string word, int i) 
+        static string ReversedWordInner(string word, int i)
         {
             if (i == 0)
                 return "" + word[i];
 
-            return word[i] + ReversedWordInner(word, i - 1); 
+            return word[i] + ReversedWordInner(word, i - 1);
 
+        }
+
+        static int Mult(int a, int b)
+        {
+            if (b == 1)
+            {
+                return a;
+            }
+            return Mult(a, b - 1) + a;
+        }
+
+        static int Fibo(int n)
+        {
+            if (n == 1)
+            {
+                return 0;
+            }
+            else if (n == 2)
+            {
+                return 1;
+            }
+            return Fibo(n - 1) + Fibo(n - 2);
+        }
+
+        static int Max(int[] arr, int n)
+        {
+            if (n == 0)
+                return arr[0];
+            return Math.Max(arr[n], Max(arr, n - 1));
+        }
+
+
+        static int Factorial(int n)
+        {
+            if(n == 0)
+                return 1;
+
+            return n * Factorial(n-1);
+        }
+
+        static int CountLines(int[,] arr, int n, int i)
+        {
+            if (i == 0)
+            {
+                if (IsNunOnLine(arr, n, i, arr.GetLength(1) - 1))
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            if (IsNunOnLine(arr, n, i, arr.GetLength(1) - 1))
+            {
+                return 1 + CountLines(arr, n, i - 1); ;
+            }
+            else
+            {
+                return 0 + CountLines(arr, n, i - 1);
+            }
+        }
+
+        static bool IsNunOnLine(int[,] arr, int n, int i, int j)
+        {
+            if (j == 0)
+            {
+                return arr[i, j] == n;
+            }
+
+            if (arr[i,j] == n)
+            {
+                return true;
+            }
+            return IsNunOnLine(arr, n, i, j - 1);
         }
     }
 }
