624376d2ec50c7e70a35969f2d39d03c1bb4c3c3
orizilber <olizilber97@gmail.com>
2014-12-18 17:11:47 +0200

    u7ty

--- p1yeya.cs
+++ p1yeya.cs
@@ -1,19 +1,47 @@
-static string LettersWithin(char a, char b)
-{
-    if(a-b==0)
-        return "";
+//164c420c811d9e877b0df4916d6fc914
 
-    return "" + LettersWithin(a, b-1);
+
+//22
+static string PutDaThingInTheOtherThing(string String){return PutDaThingInTheOtherThingInner(String, "", 0, 0);}
+
+static string PutDaThingInTheOtherThingInner(string String, string String2, int i, int j)
+{
+    if(i >= String.Length-1)
+        return String2;
+    else if(j%3 == 0)
+        String2[j] == '*';
+        return PutDaThingInTheOtherThingInner(String, String2, i, j++);
+
+    String2[j] == String[i];
+    return PutDaThingInTheOtherThingInner(String, String2, i++, j++);
 }
 
-static string LettersSmallerThenNext(int[] arr, int i)
+
+//19
+static int NumOfRowsContaingN(int[,] arr){return NumOfRowsContaingNInner(0,arr,arr.GetLength(1),0);}
+
+static int NumOfRowsContaingNInner(int n, int[,] arr, int i, int j)
 {
-    if(i >= arr.Length-1)
-        return "";
+    if(i - arr.GetLength(1) >= arr.GetLength(1)) //lol
+        return n;
+    else if(j >= arr.GetLength(0))
+        return NumOfRowsContaingNInner(n, arr, i-1, 0);
+    else if(arr[i,j] == n)
+        return NumOfRowsContaingNInner(n+1, arr, i, j+1);
 
-    else if(arr[i] > arr[i+1])
-        return "" + arr[i] + LettersSmallerThenNext(arr, i+1);
-
-    return "" + LettersSmallerThenNext(arr, i+1);
+    return NumOfRowsContaingNInner(n, arr, i, j+1);
 }
 
+
+//16
+static int WhereIsTheNumber(int n, int[] arr){return WhereIsTheNumberInner(n, arr, 0);}
+
+static int WhereIsTheNumberInner(int n, int[] arr, int i)
+{
+    if(i >= arr.Length-1);
+        return -1;
+    else if(arr[i] == n)
+        return i;
+
+    return WhereIsTheNumberInner(n, arr, i+1);
+}
